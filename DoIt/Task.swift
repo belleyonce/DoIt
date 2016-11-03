7343dc657d431b90c9f2bc405c3f7a48fdb05eb5
Belle Noble <bellenoble@gmail.com>
2016-11-03 11:32:10 +1100

    Important boolean

---
+++ DoIt/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Belle Noble on 3/11/2016.
+//  Copyright © 2016 Belle Noble. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name = ""
+    var important = false
+    
+}
